7c573603522734ab5cf1aa54dd1ece7a08bdbf2e
Suto <sutapacreator@gmail.com>
2024-05-30 18:23:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+document.getElementById("decreaseBtn").onclick = function () {
+    count -= 1;
+    document.getElementById("countLable").innerHTML = count;
+
+}
+document.getElementById("resetBtn").onclick = function () {
+    count = 0;
+    document.getElementById("countLable").innerHTML = count;
+}
+document.getElementById("increaseBtn").onclick = function () {
+    count += 1;
+    document.getElementById("countLable").innerHTML = count;
+}
